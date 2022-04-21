14e569ba8195a38b4490e9e2d2c14ae798635a9f
Nick Lazaga <nicklazaga@berkeley.edu>
2022-04-21 16:25:15 -0700

    updated cognito config settings

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_Dt1bqjcel', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7bl48h2rvgfjr4gqslrlumk7s8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
